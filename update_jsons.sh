bf118d705d493d42f0937d126d7e7ca9e4f2c45d
OldSchoolRSTips <50460610+OldSchoolRSTips@users.noreply.github.com>
2019-05-22 22:40:54 -0500

    Add files via upload

---
+++ update_jsons.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl https://raw.githubusercontent.com/OldSchoolRSTips/OldSchoolRSTips/master/tips_noob.json > /var/www/html/tips_noob.json
+curl https://raw.githubusercontent.com/OldSchoolRSTips/OldSchoolRSTips/master/tips_general.json > /var/www/html/tips_general.json
+curl https://raw.githubusercontent.com/OldSchoolRSTips/OldSchoolRSTips/master/tips_f2p.json > /var/www/html/tips_f2p.json
+curl https://raw.githubusercontent.com/OldSchoolRSTips/OldSchoolRSTips/master/tips_iron.json > /var/www/html/tips_iron.json
+curl https://raw.githubusercontent.com/OldSchoolRSTips/OldSchoolRSTips/master/tips_pvp.json > /var/www/html/tips_pvp.json
